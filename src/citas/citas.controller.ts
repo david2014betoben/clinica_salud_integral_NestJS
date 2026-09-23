9925f6f9365ed7c0e84aba6c955b06dac906c97a
david2014betoben <davidgerardonunezrojas@gmail.com>
2026-09-23 18:42:13 -0400

    feat: interceptor global de logging

--- src/citas/citas.controller.ts
+++ src/citas/citas.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { CitasService } from './citas.service.js';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { Roles } from '../auth/decorators/roles.decorator.js';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard.js';
+import { RolesGuard } from '../auth/guards/roles.guard.js';
 
+@ApiBearerAuth()
 @Controller('citas')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles('RECEPCIONISTA')
 export class CitasController {
   constructor(private readonly citasService: CitasService) {}
 
+  @ApiOperation({ summary: 'Agregar una nueva cita' })
   @Post()
   create(@Body() body: any) {
     return this.citasService.create(body);
   }
 
+  @ApiOperation({ summary: 'Lista todas las citas creadas' })
   @Get()
   findAll() {
     return this.citasService.findAll();
